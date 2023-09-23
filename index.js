186c30292e43e4d71bb66d9ab1d9b3be1ac86756
KELVIN MURITHI <kelvinmurithi@live.com>
2023-09-23 12:18:40 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function() {
+    return 2000 / 100;
+};
+
+const square = (value) => {
+    return value * value;
+};
+
+const add = (parameter1, parameter2) => {
+    return parameter1 + parameter2;
+}
